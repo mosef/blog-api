e6962e14ddfede94f387d7b5c6141e391ee80654
Mosef Asad <mosefasad@gmail.com>
2017-10-23 00:41:38 -0400

    updated project

--- models.js
+++ models.js
@@ -1,55 +1,30 @@
-const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
+const blogPostSchema = mongoose.Schema({
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogPostSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    author: this.authorName,
+    content: this.content,
+    title: this.title,
+    created: this.created
+  };
 }
 
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
-  }
-};
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
-}
-
-module.exports = {BlogPosts: createBlogPostsModel()};
+module.exports = {BlogPost};
